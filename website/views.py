4edeff069ebcd20c26f50a2a088459f1a850ebb4
Tanay Sandeep Punjabi <tp560@vi.cs.rutgers.edu>
2022-04-11 23:22:41 -0400

    Visual Enhancements

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, flash, render_template, request
+from matplotlib.pyplot import legend, title
 from . import mysql
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 import json
 import plotly
 
@@ -10,7 +11,8 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        query_s = request.form.get('query')
+        grade_col = {'A':'#1f77b4', 'B':'#ff7f0e', 'C':'#2ca02c', 'D':'#d62728', 'E':'#9467bd', 'F':'#8c564b'}
+        
         query_o = "SELECT * FROM Moody2022_new;"
 
         cur = mysql.connection.cursor()
@@ -18,18 +20,44 @@ def home():
         cur.execute(query_o)
         df = pd.DataFrame(cur.fetchall())
         print(len(df))
-        fig = px.box(df, y="SCORE", x="GRADE")
+        fig = go.Figure()
+        for grade in grade_col:
+            fig.add_trace(go.Box(y=df[df.GRADE.eq(grade)]['SCORE'], name=grade, 
+            marker_color=grade_col[grade]))
+        fig.update_layout(
+            title=query_o,
+            legend_title="GRADES")
 
         graphJSON_o = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-        cur.execute(query_s)
-        df = pd.DataFrame(cur.fetchall())
-        print(df.head())
-        fig = px.box(df, y="SCORE", x="GRADE")
+        if request.form['submit_btn'] == 'sq1':
+            query_s = "SELECT * FROM Moody2022_new WHERE DOZES_OFF='always';"
+        elif request.form['submit_btn'] == 'sq2':
+            query_s = "SELECT * FROM Moody2022_new WHERE PARTICIPATION < 0.2;"
+        elif request.form['submit_btn'] == 'sq3':
+            query_s = "SELECT * FROM Moody2022_new WHERE PARTICIPATION > 0.5 and TEXTING_IN_CLASS='never';"
+        else:
+            query_s = request.form.get('query')
+        
+        try:
+            
+            cur.execute(query_s)
+            df = pd.DataFrame(cur.fetchall())
+            print(df.head())
 
-        graphJSON_s = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+            fig = go.Figure()
+            for grade in grade_col:
+                fig.add_trace(go.Box(y=df[df.GRADE.eq(grade)]['SCORE'], name=grade, 
+                marker_color=grade_col[grade]))
+            fig.update_layout(
+                title=query_s,
+                legend_title="GRADES")        
+
+            graphJSON_s = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-        return render_template("base.html", graphJSON_o=graphJSON_o, graphJSON_s=graphJSON_s)
+            return render_template("base.html", graphJSON_o=graphJSON_o, graphJSON_s=graphJSON_s)
+        except:
+            flash("Incorrect query", category='error')
 
     return render_template("base.html")
