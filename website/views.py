9c60abe5f6dc02885a7e5edc0716ddc5cf51370c
Tanay Sandeep Punjabi <tp560@vi.cs.rutgers.edu>
2022-04-12 21:12:40 -0400

    Charting mistake corrected

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from flask import Blueprint, flash, render_template, request
 from matplotlib.pyplot import legend, title
 from . import mysql
@@ -10,25 +11,20 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    dataset = pd.read_csv('website/moody2022_new.csv').to_json(orient='records')
+
     if request.method == 'POST':
-        grade_col = {'A':'#1f77b4', 'B':'#ff7f0e', 'C':'#2ca02c', 'D':'#d62728', 'E':'#9467bd', 'F':'#8c564b'}
         
         query_o = "SELECT * FROM Moody2022_new;"
 
         cur = mysql.connection.cursor()
         
         cur.execute(query_o)
-        df = pd.DataFrame(cur.fetchall())
-        print(query_o, "executed successfully. Shape of results - ", df.shape)
-        fig = go.Figure()
-        for grade in grade_col:
-            fig.add_trace(go.Box(y=df[df.GRADE.eq(grade)]['SCORE'], name=grade, 
-            marker_color=grade_col[grade]))
-        fig.update_layout(
-            title=query_o,
-            legend_title="GRADES")
+        df_o = pd.DataFrame(cur.fetchall())
+        df_o['src'] = 0
+        print(query_o, "executed successfully. Shape of results - ", df_o.shape)
 
-        graphJSON_o = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
 
         if request.form['submit_btn'] == 'sq1':
             query_s = "SELECT * FROM Moody2022_new WHERE DOZES_OFF='always';"
@@ -42,22 +38,44 @@ def home():
         try:
             
             cur.execute(query_s)
-            df = pd.DataFrame(cur.fetchall())
-            print(query_s, "executed successfully. Shape of results", df.shape)
+            df_s = pd.DataFrame(cur.fetchall())
+            df_s['src'] = 1
+            print(query_s, "executed successfully. Shape of results", df_s.shape)
+            df_s = pd.concat([df_o, df_s])
 
             fig = go.Figure()
-            for grade in grade_col:
-                fig.add_trace(go.Box(y=df[df.GRADE.eq(grade)]['SCORE'], name=grade, 
-                marker_color=grade_col[grade]))
+            fig.add_trace(go.Box(
+                y=df_s[df_s['src']==0]['SCORE'], 
+                x=df_s[df_s['src']==0]['GRADE'],
+                name="Zero", 
+                marker_color="#1f77b4"
+                ))
+            
+            print(df_s[df_s['src']==1]['SCORE'].shape)
+
+            fig.add_trace(go.Box(
+                y=df_s[df_s['src']==1]['SCORE'], 
+                x=df_s[df_s['src']==1]['GRADE'],
+                name="Sliced", 
+                marker_color="#ff7f0e"
+                ))
+            
             fig.update_layout(
                 title=query_s,
-                legend_title="GRADES")        
+                legend_title="GRADES",
+                boxmode='group'
+                )
+
+            fig.update_xaxes(
+                categoryorder='array',
+                categoryarray = ['A', 'B', 'C', 'D', 'F']
+            )        
 
             graphJSON_s = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-            return render_template("base.html", graphJSON_o=graphJSON_o, graphJSON_s=graphJSON_s)
+            return render_template("base.html", graphJSON_s=graphJSON_s, dataset=dataset)
         except:
             flash("Incorrect query", category='error')
 
-    return render_template("base.html")
+    return render_template("base.html", dataset=dataset)
